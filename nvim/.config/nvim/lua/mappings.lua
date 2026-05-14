dad450e30021c12bc98fbb35ab0d7d1d9fccbc27
CrisDan1905 <danilo1905@outlook.com>
2026-05-14 21:54:06 +0000

    Add gemini cli shortcut to nvim

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -7,4 +7,12 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>ga", function()
+  require("nvchad.term").toggle({
+    pos = "float",
+    id = "'geminiAgent",
+    cmd = "gemini"
+  })
+end, { desc = "Gemini Agent" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
